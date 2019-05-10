c1a8c3b7611929f0456946ad3a2ad841509657c9
KYLE T STEFFY <kylesteffy@gmail.com>
2019-05-10 12:17:20 -0400

    Add files via upload

---
+++ PigLatinTranslator/PigLatinTranslator/PigLatinTranslator.cpp
@@ -0,0 +1,15 @@
+// PigLatinTranslator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string word;
+
+	cout << "Type: " << endl;
+	cin >> word;
+	cout << "You Typed: " << word;
+}
